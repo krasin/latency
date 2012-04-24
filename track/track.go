23692fd08bd2077803eebe0e42941c78ccf6c925
Ivan Krasin <imkrasin@gmail.com>
2012-04-24 16:22:46 +0000

    Emit json reports to stderr in latency/track

--- track/track.go
+++ track/track.go
@@ -1,6 +1,8 @@
 package track
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/krasin/latency"
 	"log"
 	"time"
@@ -11,9 +13,23 @@ var ticker = time.NewTicker(30 * time.Second).C
 
 var defaultTracker = latency.NewTracker(reports, ticker)
 
+type LatencyReport struct {
+	Latency map[string]int
+}
+
 func logReports() {
 	for report := range reports {
-		log.Printf("Latency report: %v", report)
+		lr := LatencyReport{Latency: make(map[string]int)}
+		for lat, count := range report {
+			lr.Latency[fmt.Sprintf("%s", lat)] = count
+		}
+		data, err := json.Marshal(lr)
+		if err != nil {
+			// TODO: do not emit incorrect json in case if error contains `"`.
+			log.Printf(`Latency report: { "error": "%v" }`, err)
+			continue
+		}
+		log.Printf("Latency report: %v", string(data))
 	}
 }
 
