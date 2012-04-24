218f10ea64ef7253d52726cf9593581372d70d4d
Ivan Krasin <imkrasin@gmail.com>
2012-04-24 06:09:21 +0000

    Use raw channel for ticker in the test. Test pass in 11 ms

--- latency_test.go
+++ latency_test.go
@@ -7,16 +7,18 @@ import (
 
 func TestOneRequest(t *testing.T) {
 	reports := make(chan LatencyReport)
-	ticker := time.NewTicker(20 * time.Millisecond)
-	defer ticker.Stop()
-	lat := NewTracker(reports, ticker.C)
+	ticker := make(chan time.Time)
+
+	lat := NewTracker(reports, ticker)
 	defer lat.Stop()
 	track := lat.Track()
 	track()
+	ticker <- time.Now().UTC()
 	report := <-reports
 	if len(report) != 1 {
 		t.Fatalf("Expected: 1 element in the report, has: %d elements", len(report))
 	}
+	ticker <- time.Now().UTC()
 	report = <-reports
 	if len(report) != 0 {
 		t.Fatalf("Expected: 1 element in the report, has: %d elements", len(report))
