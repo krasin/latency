956f485a66d7f9bd5a45b12509e0fbee221e1a89
Ivan Krasin <imkrasin@gmail.com>
2012-04-24 06:24:48 +0000

    Add latency/track package that provides an easy and preconfigured interface

---
+++ track/track.go
@@ -0,0 +1,26 @@
+package track
+
+import (
+	"github.com/krasin/latency"
+	"log"
+	"time"
+)
+
+var reports = make(chan latency.LatencyReport)
+var ticker = time.NewTicker(30 * time.Second).C
+
+var defaultTracker = latency.NewTracker(reports, ticker)
+
+func logReports() {
+	for report := range reports {
+		log.Printf("Latency report: %v", report)
+	}
+}
+
+func init() {
+	go logReports()
+}
+
+func Track() Reporter {
+	return defaultTracker.Track()
+}
